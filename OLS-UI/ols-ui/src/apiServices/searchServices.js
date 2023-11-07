b39b7cad581371b1b534e8a5aac44747aea21900
Bùi Văn Kiên <kbui0212@gmail.com>
2023-11-07 12:30:59 +0700

    OLS-UI using axios, apiServices, building search

---
+++ OLS-UI/ols-ui/src/apiServices/searchServices.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as request from '~/utils/request';
+
+export const search = async (q, type = 'less') => {
+    try {
+        const res = await request.get(`users/search`, {
+            params: {
+                q,
+                type,
+            },
+        });
+        return res.data;
+    } catch (error) {
+        console.log(error);
+    }
+};
