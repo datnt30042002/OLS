471dde5da099203de9908af3ae39ed5e00164f5b
Bùi Văn Kiên <kbui0212@gmail.com>
2023-11-08 10:53:21 +0700

    OLS-UI clean code - optimize components - header

--- OLS-UI/ols-ui/src/components/Popper/Menu/Menu.js
+++ OLS-UI/ols-ui/src/components/Popper/Menu/Menu.js
@@ -33,6 +33,7 @@ const Menu = ({ children, items = [], hideOnClick = false, onChange = defaultFun
                         if (isParent) {
                             // consolelog test
                             //console.log(item.children);
+                            // next page
                             setHistory((prev) => [...prev, item.children]);
                         } else {
                             onChange(item);
@@ -43,32 +44,47 @@ const Menu = ({ children, items = [], hideOnClick = false, onChange = defaultFun
         });
     };
 
+    // hanldeback menu
+    const handleBack = () => {
+        // đơn giản là xóa phần tử cuối đi nó sẽ lùi lại 1 cấp
+        setHistory((prev) => prev.slice(0, prev.length - 1));
+    };
+
+    // render result
+    const renderResult = (attrs) => (
+        <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+            <PopperWrapper className={cx('menu-popper')}>
+                {history.length > 1 && (
+                    <Header
+                        title={current.title}
+                        // có nghĩa sau khi đi vào menu con của language -> nhấn icon back sẽ quay lại menu cha
+                        // tính chất của array [0, 1, 2, ...]
+                        onBack={handleBack}
+                    />
+                )}
+                <div className={cx('menu-body')}>{renderItems()}</div>
+            </PopperWrapper>
+        </div>
+    );
+
+    // reset to first page - menu
+    // trả về trang đầu tiên - menu
+    const hanldeReset = () => {
+        setHistory((prev) => prev.slice(0, 1));
+    };
+
     return (
         <Tippy
-            //visible //nếu chọn visible thì có thể hiện mặc định
+            //visible //nếu chọn visible thì có thể hiển thị mặc định
             interactive
             delay={[0, 700]}
             offset={[30, 14]} // căn độ lệch cho menu avatar - chieu ngang, chieu cao
             hideOnClick={hideOnClick} // ẩn khi click - ở đây là menu của avtar
             placement="bottom-end"
-            render={(attrs) => (
-                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx('menu-popper')}>
-                        {history.length > 1 && (
-                            <Header
-                                title={current.title}
-                                onBack={() => {
-                                    // đơn giản là xóa phần tử cuối đi nó sẽ lùi lại 1 cấp
-                                    setHistory((prev) => prev.slice(0, prev.length - 1));
-                                }}
-                            />
-                        )}
-                        <div className={cx('menu-body')}>{renderItems()}</div>
-                    </PopperWrapper>
-                </div>
-            )}
+            render={renderResult}
             // set về phần tử đầu tiên -> khi bỏ hover sẽ trả về
-            onHide={() => setHistory((prev) => prev.slice(0, 1))}
+            // khi hover avatar -> đi vào menu con như language -> bỏ hover -> mất -> quany lại menu cha
+            onHide={hanldeReset}
         >
             {children}
         </Tippy>
--- OLS-UI/ols-ui/src/layouts/components/Search/Search.js
+++ OLS-UI/ols-ui/src/layouts/components/Search/Search.js
@@ -20,18 +20,18 @@ const Search = () => {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
     // show khu vuực kết quả Search lên với 2 ddkien, có ký tự và phải đc focus
-    const [showResult, setShowResult] = useState(true);
+    const [showResult, setShowResult] = useState(false);
     const [loading, setLoading] = useState(false);
 
     // 1: '' lần đầu tiên component này chạy là 1 chuỗi rỗng
     // 2: 'h'
-    const debounced = useDebounce(searchValue, 500);
+    const debouncedValue = useDebounce(searchValue, 500);
 
     const inputRef = useRef();
 
     // get api
     useEffect(() => {
-        if (!debounced.trim()) {
+        if (!debouncedValue.trim()) {
             setSearchResult([]);
             return;
         }
@@ -39,14 +39,14 @@ const Search = () => {
         const fetchApi = async () => {
             setLoading(true);
 
-            const result = await searchServices.search(debounced);
+            const result = await searchServices.search(debouncedValue);
             setSearchResult(result);
 
             setLoading(false);
         };
 
         fetchApi();
-    }, [debounced]);
+    }, [debouncedValue]);
 
     const handleClear = () => {
         // text input
@@ -70,6 +70,7 @@ const Search = () => {
     };
 
     return (
+        // Fix Tippy warning
         // Using a wrapper <div> or <span> tag around the reference element solves this by creating a new parentNode context. Specifying `appendTo: document.body` silences this warning, but it assumes you are using a focus management solution to handle keyboard navigation.
         <div>
             {/* Tippy: hỗ trợ hiển thị hiển thị khung list khi hover - tooltip */}
@@ -82,6 +83,8 @@ const Search = () => {
                     <PopperWrapper>
                         <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                             <h4 className={cx('search-title')}>Accounts</h4>
+                            {/* tối ưu cho trường hợp quá nhiều elements
+                            -> gây ảnh hưởng đến hiệu năng */}
                             {searchResult.map((result) => (
                                 <AccountItem key={result.id} data={result} />
                             ))}
